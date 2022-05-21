7d35110264f4d2ef9b5d4c52fb35ffe7180d4338
andry81 <andry@inbox.ru>
2022-05-21 17:28:56 +0300

    2022.05.21: * fixed: bash: inpage/accum-downloads.sh, boards/accum-stats.sh: accidental exit with 0 on curl error

--- bash/board/accum-stats.sh
+++ bash/board/accum-stats.sh
@@ -79,7 +79,7 @@ else
 
   gh_enable_print_buffering
 
-  (( ! CONTINUE_ON_INVALID_INPUT )) && exit $?
+  (( ! CONTINUE_ON_INVALID_INPUT )) && exit 255
 fi
 
 # check on empty
@@ -91,7 +91,7 @@ if [[ ! -s "$TEMP_DIR/response.txt" ]]; then
 
   gh_enable_print_buffering
 
-  (( ! CONTINUE_ON_INVALID_INPUT )) && exit $?
+  (( ! CONTINUE_ON_INVALID_INPUT )) && exit 255
 fi
 
 replies="$(sed -rn "$replies_sed_regexp" "$TEMP_DIR/response.txt")"
